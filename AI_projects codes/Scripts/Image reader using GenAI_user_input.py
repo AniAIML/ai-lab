c02803935f5d0e6e960479914d83622d3bae8e95
AI Lab <aniruddha.garain@gmail.com>
2026-04-11 15:38:40 +0530

    GenAI projects scripts and datasets

---
+++ AI_projects codes/Scripts/Image reader using GenAI_user_input.py
@@ -0,0 +1,25 @@
+from IPython.display import display, Audio
+import google.generativeai as genai
+from google.colab import files
+from PIL import Image
+import matplotlib.pyplot as plt
+
+genai.configure(api_key="AIzaSyBAuXJkZf-uGgjs75lddcPzke8TFV_UQQk")
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+uploaded = files.upload()
+image_path = list(uploaded.keys())[0]
+image = Image.open(image_path)
+
+while True:
+    q = input("\nAsk something about the image (or type 'exit'): ")
+    if q.lower() == "exit":
+        break
+
+    ans = model.generate_content([image, q])
+    print("\nAnswer:\n", ans.text)
+for i in response.text:
+  if i.isalpha()==True or i==" " or i=="\n":
+    response1=response1+i
+response=response1
+print(response)
